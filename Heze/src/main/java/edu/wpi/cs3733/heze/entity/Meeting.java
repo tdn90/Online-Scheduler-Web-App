5f4282c9bfaef64c21f61ab463a6738ddb71e07c
Minh Pham <minhpham@autoreg-144169.dyn.wpi.edu>
2018-11-28 00:55:56 -0500

    Save changes

--- Heze/src/main/java/edu/wpi/cs3733/heze/entity/Meeting.java
+++ Heze/src/main/java/edu/wpi/cs3733/heze/entity/Meeting.java
@@ -1,5 +1,50 @@
 package edu.wpi.cs3733.heze.entity;
 
 public class Meeting {
+	String id;
+	TimeSlot tl;
+	String participant;
+	String secretKey;
+	
+	public Meeting(String id, TimeSlot tl, String participant, String secretKey) {
+		this.id = id;
+		this.tl = tl;
+		this.participant = participant;
+		this.secretKey = secretKey;
+	}
 
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public TimeSlot getTl() {
+		return tl;
+	}
+
+	public void setTl(TimeSlot tl) {
+		this.tl = tl;
+	}
+
+	public String getParticipant() {
+		return participant;
+	}
+
+	public void setParticipant(String participant) {
+		this.participant = participant;
+	}
+
+	public String getSecretKey() {
+		return secretKey;
+	}
+
+	public void setSecretKey(String secretKey) {
+		this.secretKey = secretKey;
+	}
+	
+	
+	
 }
--- Heze/src/main/java/edu/wpi/cs3733/heze/entity/Time.java
+++ Heze/src/main/java/edu/wpi/cs3733/heze/entity/Time.java
@@ -1,5 +1,13 @@
 package edu.wpi.cs3733.heze.entity;
 
 public class Time {
-
+	int hour;
+	int minute;
+	int second;
+	
+	public Time(int hour, int minute, int second) {
+		this.hour = hour;
+		this.minute = minute;
+		this.second = second;
+	}
 }
