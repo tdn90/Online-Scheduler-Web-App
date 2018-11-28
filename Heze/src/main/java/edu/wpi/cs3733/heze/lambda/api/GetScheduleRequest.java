36bb60c94fe079d7ddf6fd888c49667f751c6b28
alescion <33096482+alescion@users.noreply.github.com>
2018-11-27 22:18:53 -0500

    Implemented GetSchedule request and changed dependencies in pom.xml

--- Heze/src/main/java/edu/wpi/cs3733/heze/lambda/api/GetScheduleRequest.java
+++ Heze/src/main/java/edu/wpi/cs3733/heze/lambda/api/GetScheduleRequest.java
@@ -1,5 +1,14 @@
 package edu.wpi.cs3733.heze.lambda.api;
 
 public class GetScheduleRequest {
-
+	public String secretkey;
+	
+	public GetScheduleRequest(String k)
+	{
+		this.secretkey = k;
+	}
+	
+	public String toString() {
+		return "CreateSchedule(" + secretkey + ")";
+	}
 }
