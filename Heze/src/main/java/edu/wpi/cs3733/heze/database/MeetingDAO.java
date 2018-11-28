2e8878c86d00ebc6ea0472e18620851d5ef49c49
tdn90 <nguyentridung1212@gmail.com>
2018-11-28 00:57:03 -0500

    Initialize MeetingDAO stub

--- Heze/src/main/java/edu/wpi/cs3733/heze/database/MeetingDAO.java
+++ Heze/src/main/java/edu/wpi/cs3733/heze/database/MeetingDAO.java
@@ -1,5 +1,39 @@
 package edu.wpi.cs3733.heze.database;
 
+import java.sql.*;
+
+import edu.wpi.cs3733.heze.entity.Meeting;
+
 public class MeetingDAO {
-	
+	java.sql.Connection conn;
+
+    public MeetingDAO() {
+    	try  {
+    		conn = DatabaseUtil.connect();
+    	} catch (Exception e) {
+    		conn = null;
+    	}
+    }
+    
+	public Meeting getConstant(String name) throws Exception {
+
+		try {
+			Meeting constant = null;
+			PreparedStatement ps = conn.prepareStatement("SELECT * FROM Constants WHERE name=?;");
+			ps.setString(1, name);
+			ResultSet resultSet = ps.executeQuery();
+
+			while (resultSet.next()) {
+				//constant = generateConstant(resultSet);
+			}
+			resultSet.close();
+			ps.close();
+
+			return constant;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new Exception("Failed in getting constant: " + e.getMessage());
+		}
+	}
 }
