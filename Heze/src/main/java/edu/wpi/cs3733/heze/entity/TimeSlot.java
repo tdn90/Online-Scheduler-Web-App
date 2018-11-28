5872101e17177e86b5b63b1cafc6c7e55cea42a2
tdn90 <nguyentridung1212@gmail.com>
2018-11-28 00:57:27 -0500

    Add TimeSlot entity stub

---
+++ Heze/src/main/java/edu/wpi/cs3733/heze/entity/TimeSlot.java
@@ -0,0 +1,23 @@
+package edu.wpi.cs3733.heze.entity;
+
+public class TimeSlot {
+	String timeslotID;
+	Time startTime; 
+	int meetingDuration; 
+	Meeting meeting; 
+	boolean organizerAvailable; 
+	
+	public TimeSlot(String timeslotID, Time startTime, int meetingDuration, Meeting meeting,
+			boolean organizerAvailable) {
+		super();
+		this.timeslotID = timeslotID;
+		this.startTime = startTime;
+		this.meetingDuration = meetingDuration;
+		this.meeting = meeting;
+		this.organizerAvailable = organizerAvailable;
+	}
+	
+	boolean hasMeeting() {
+		return meeting != null;
+	}
+}
