e76968e7879d9279a11c1edfd69dcaf2093ecb15
tdn90 <nguyentridung1212@gmail.com>
2018-11-28 01:16:44 -0500

    Add Schedule entity

---
+++ Heze/src/main/java/edu/wpi/cs3733/heze/entity/Schedule.java
@@ -0,0 +1,30 @@
+package edu.wpi.cs3733.heze.entity;
+
+import java.util.List;
+
+public class Schedule {
+	
+	String scheduleID;
+	List<ScheduleDate> days; 
+	String schedule_secretKey; 
+	String name; 
+	Time startTime; 
+	Time endTime; 
+	int meetingDuration; 
+	
+	public Schedule(String scheduleID, List<ScheduleDate> days, String schedule_secretKey, String name, Time startTime,
+			Time endTime, int meetingDuration) {
+		super();
+		this.scheduleID = scheduleID;
+		this.days = days;
+		this.schedule_secretKey = schedule_secretKey;
+		this.name = name;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.meetingDuration = meetingDuration;
+	}
+	
+	public Iterator<ScheduleDate> daysIt() {
+		return days.iterator();
+	}
+}
