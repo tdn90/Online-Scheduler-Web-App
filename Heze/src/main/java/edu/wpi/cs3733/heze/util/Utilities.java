6a1f3257a6098db45c9387ac57a6f48025f808c4
alescion <33096482+alescion@users.noreply.github.com>
2018-11-28 10:51:10 -0500

    Added util package with static key generating method

---
+++ Heze/src/main/java/edu/wpi/cs3733/heze/util/Utilities.java
@@ -0,0 +1,24 @@
+package edu.wpi.cs3733.heze.util;
+
+import java.util.UUID;
+
+public class Utilities {
+	
+	/**
+	 * 
+	 * @param len The length of the String to be generated
+	 * @return A pseudorandom String of length len (maximum 32 characters)
+	 */
+	public static String generateKey(int len)
+	{
+		String uniqueID = UUID.randomUUID().toString().replace("-", "");
+		
+		if (len >= 0 && len < 32 && uniqueID.length() == 32)
+		{
+			return uniqueID.substring(0, uniqueID.length() - (32 - len));
+		}
+		
+		return uniqueID;
+	}
+	
+}
