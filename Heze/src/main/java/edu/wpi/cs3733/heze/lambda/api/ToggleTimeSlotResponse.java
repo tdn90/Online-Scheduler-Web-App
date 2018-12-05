04412734968ed13176d9193dcf8a2a5c45e3aca2
tdn90 <nguyentridung1212@gmail.com>
2018-12-04 20:16:48 -0500

    Add API classes needed for toggling timeslot

---
+++ Heze/src/main/java/edu/wpi/cs3733/heze/lambda/api/ToggleTimeSlotResponse.java
@@ -0,0 +1,18 @@
+package edu.wpi.cs3733.heze.lambda.api;
+
+public class ToggleTimeSlotResponse {
+	public int httpCode; 
+	public boolean isTimeSlotAvailable; 
+	
+	public ToggleTimeSlotResponse(int httpCode) {
+		this.httpCode = httpCode;
+	}
+	
+	public ToggleTimeSlotResponse(int httpCode, boolean isAvailable) {
+		this.isTimeSlotAvailable = isAvailable;
+	}
+	
+	public String toString() {
+		return "Response to Toggle Time Slot => (code:" + this.httpCode + "\nAvailability:" + this.isTimeSlotAvailable + ")";
+	}
+}
