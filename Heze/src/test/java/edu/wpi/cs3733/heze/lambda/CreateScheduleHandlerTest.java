5384a69a61fc3c100936d17ade24091fd684217c
tdn90 <nguyentridung1212@gmail.com>
2018-12-11 23:25:54 -0500

    Fix code in Testing file

--- Heze/src/test/java/edu/wpi/cs3733/heze/lambda/CreateScheduleHandlerTest.java
+++ Heze/src/test/java/edu/wpi/cs3733/heze/lambda/CreateScheduleHandlerTest.java
@@ -283,22 +283,6 @@ public class CreateScheduleHandlerTest {
         sc = organizerGetSchedule();
         assertEquals(daysbefore + daystotest, sc.getSizeofList());
 	}
-	
-	@Test
-	public void testSysAdminDeleteScheduleList() throws IOException {
-		int days = 3;
-		SysAdminDeleteScheduleHandler handler = new SysAdminDeleteScheduleHandler();
-		String req = new Gson().toJson(new SysAdminDeleteScheduleRequest(days));
-		String postable = new Gson().toJson(new PostRequest(req));
-		
-		InputStream input = new ByteArrayInputStream(postable.getBytes());
-        OutputStream output = new ByteArrayOutputStream();
-        
-        handler.handleRequest(input, output, createContext("delete meeting list"));
-        TestingResponse response_holder = new Gson().fromJson(output.toString(), TestingResponse.class);
-        SysAdminDeleteScheduleResponse response = new Gson().fromJson(response_holder.body, SysAdminDeleteScheduleResponse.class);
-        assertEquals(200, response.httpCode);
-	}
         
 	@Test 
 	public void testGetOpenSlotsHandler() throws IOException {
